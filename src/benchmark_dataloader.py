242359bae6acf7b090cbc40fa9ffb128a7b72ad2
madratman <ratneshmadaan@gmail.com>
2022-06-21 02:32:22 +0000

    pytorch profiler with schedule option

--- src/benchmark_dataloader.py
+++ src/benchmark_dataloader.py
@@ -1,101 +1,53 @@
-import argparse
+import time
 
 from data import build_loader
+from utils.opts import parse_args
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--print_freq", type=int, default=100)
-    parser.add_argument("--save_freq", type=int, default=100)
-    parser.add_argument("--work_dir", type=str, default="work_dirs/pretrain/rgb2rgb")
-    parser.add_argument("--resume", type=str, default="")
-    parser.add_argument("--epochs", type=int, default=800)
-    parser.add_argument("--warmup_ratio", type=float, default=0.05)
-    parser.add_argument("--backbone", type=str, default="vit")
-    parser.add_argument("--local_rank", type=int, default=0)
-
-    # ================================== added arguments for TaratanAIR ==============
-    parser.add_argument(
-        "--flow", default=False, type=bool, help="indicate select optical flow or not"
-    )
-    parser.add_argument("--img", default=False, type=bool)
-    parser.add_argument("--dataloader_mode", default="all", type=str)
-    parser.add_argument("--visual_aug", default=False, type=bool)
-    parser.add_argument("--flip", default=False, type=bool)
-    parser.add_argument(
-        "--use_memory", default=False, help="use multimodal fusion memory"
-    )
-    parser.add_argument(
-        "--use_external", default=False, type=bool, help="use memory as external info"
-    )
-    parser.add_argument(
-        "--use_pred", default=False, type=bool, help="use predictive contrastive loss."
-    )
-    parser.add_argument(
-        "--use_inst", default=False, type=bool, help="use instance contrastive loss."
-    )
-    parser.add_argument(
-        "--use_img2img", default=False, type=bool, help="compute img2img loss"
-    )
-    parser.add_argument(
-        "--use_flow_both",
-        default=False,
-        type=bool,
-        help="training rgb->flow both pred and instance.",
-    )
-    parser.add_argument(
-        "--flow_inst_only",
-        default=False,
-        type=bool,
-        help="only use instance loss for flow.",
-    )
-    # parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file', )
-    parser.add_argument("--use_val", default=False, type=bool)
-    # parser.add_argument('--data_path', default= '../Data/tartanair-release1/')
-    parser.add_argument(
-        "--train_ann_file",
-        default="/datadrive/commondatasets/tartanair-release1/train_ann_debug.json",
-        type=str,
-    )
-    parser.add_argument("--val_ann_file", default=" ", type=str)
-    parser.add_argument("--train_transform", default="", type=str)
-    parser.add_argument(
-        "--train_transform_aug",
-        default="TartanAirVideoTransformWithAugmentation",
-        type=str,
-    )
-    parser.add_argument("--val_transform", default=False, type=bool)
-    parser.add_argument("--video_name_keyword", default=None)
-    parser.add_argument("--pin_memory", default=True, type=bool)
-    parser.add_argument("--img_dim", default=224, type=int)
-    parser.add_argument("--img_crop", default=448, type=int)
-    parser.add_argument(
-        "--seq_len", default=1, type=int, help="number of frames in each video block"
-    )
-    parser.add_argument(
-        "--num_seq", default=16, type=int, help="number of video blocks"
-    )
-    parser.add_argument("-j", "--workers", default=0, type=int)
-
-    args = parser.parse_args()
-    return args
+def get_tartan_dataset_and_loader(args):
+    data_type = ["image_left"]
+    if args.flow:
+        data_type.append("flow_flow")
+    print("--used modalities are:", data_type)
+    train_dataset, _, train_loader, _, _ = build_loader(args, data_type)
+    # train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    return train_dataset, train_loader
 
 
-args = parse_args()
+def benchmark(args):
+    train_dataset, train_dataloader = get_tartan_dataset_and_loader(args)
+    tic = time.time()
+    for batch_idx, _ in enumerate(train_dataloader):
+        print("batch_idx {batch_idx}")
+    print(f"time taken for {len(train_dataloader)} batches: {time.time()-tic} seconds")
 
-### ================= dataset TartanAIR ===============================================
-data_type = ["image_left"]
-if args.flow:
-    data_type.append("flow_flow")
-print("--used modalities are:", data_type)
+
+def trace_handler(p):
+    output = p.key_averages().table(sort_by="self_cuda_time_total", row_limit=10)
+    print(output)
+    p.export_chrome_trace("trace_" + str(p.step_num) + ".json")
 
 
-train_dataset, _, train_loader, _, _ = build_loader(args, data_type)
-# train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-sample = train_dataset[0]
-bla = 1
+def pytorch_profiler_schedule(args):
+    from torch.profiler import ProfilerActivity, profile, schedule
 
-for batch in train_loader:
-    print(f"new_batch")
+    train_dataset, train_dataloader = get_tartan_dataset_and_loader(args)
+    with profile(
+        activities=[ProfilerActivity.CPU],
+        profile_memory=True,
+        record_shapes=False,
+        schedule=schedule(wait=1, warmup=1, active=2),
+        on_trace_ready=trace_handler,
+    ) as prof:
+        for batch_idx, _ in enumerate(train_dataloader):
+            print(f"batch_idx {batch_idx}")
+
+
+def main(args):
+    # benchmark(args)
+    pytorch_profiler_schedule(args)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
