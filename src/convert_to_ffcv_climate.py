9ea484be33d40eed8b25cc9fceead9a1666d263d
rejuvyesh <mail@rejuvyesh.com>
2022-07-09 00:35:46 +0000

    wip datapipe support

--- src/convert_to_ffcv_climate.py
+++ src/convert_to_ffcv_climate.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from argparse import ArgumentParser
+from functools import partial
 
 import numpy as np
 import torch
@@ -8,6 +9,7 @@ import torchdata.datapipes as dp
 from ffcv.fields import NDArrayField
 from ffcv.writer import DatasetWriter
 from torch.utils.data import DataLoader
+from tqdm.contrib.concurrent import thread_map
 
 from climate.era5_datapipe import ERA5Forecast, ERA5Npy, ERA5Zarr, IndividualDataIter
 
@@ -43,6 +45,31 @@ def get_noshuffle_datapipe(path, batchsize=1, dataformat="npy"):
     return data
 
 
+def count_samples_in_pipeline(datap, pipeline):
+    count = 0
+    for dd in pipeline(datap):
+        count += 1
+    return count
+
+
+def worker_job_datapipe(
+    input_queue,
+    metadata_sm,
+):
+    # TODO: can basically follow worker_job_webdataset with appropriate changes
+    pass
+
+
+class DataPipeWriter(DatasetWriter):
+    def from_datapipe(self, datapipe, pipeline=None):
+        counter = partial(count_samples_in_pipeline, pipeline=pipeline)
+        lengths = thread_map(counter, datapipe)
+        total_len = sum(lengths)
+        offsets = np.cumsum([0] + lengths)[:-1]
+        todos = zip(datapipe, offsets)
+        self._write_common(total_len, todos, worker_job_datapipe, (pipeline,))
+
+
 def convert_climate(args):
     datap = get_noshuffle_datapipe(args.datapath, dataformat=args.dataset)
     data = None
@@ -52,12 +79,12 @@ def convert_climate(args):
             print(k, v.shape)
         break
 
-    writer = DatasetWriter(args.outfile, {k: NDArrayField(shape=v.shape, dtype=v.dtype) for k, v in data.items()})
+    writer = DataPipeWriter(args.outfile, {k: NDArrayField(shape=v.shape, dtype=v.dtype) for k, v in data.items()})
 
     def pipeline(dd):
         return dd.map(to_tuple)
 
-    writer.from_webdataset(datap, pipeline=pipeline)
+    writer.from_datapipe(datap, pipeline=pipeline)
 
 
 def parse_args():
