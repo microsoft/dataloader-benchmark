02f57f185f458989242ad3a9f169137f05fa9eeb
rejuvyesh <mail@rejuvyesh.com>
2022-07-09 21:10:17 +0000

    hack to make it "work", still fails with page_size issues

--- src/convert_to_ffcv_climate.py
+++ src/convert_to_ffcv_climate.py
@@ -7,11 +7,12 @@ import numpy as np
 import torch
 import torchdata.datapipes as dp
 from ffcv.fields import NDArrayField
-from ffcv.writer import DatasetWriter
+from ffcv.writer import DatasetWriter, handle_sample
 from torch.utils.data import DataLoader
 from tqdm.contrib.concurrent import thread_map
 
-from climate.era5_datapipe import ERA5Forecast, ERA5Npy, ERA5Zarr, IndividualDataIter
+from climate.era5_datapipe import (ERA5Forecast, ERA5Npy, ERA5Zarr,
+                                   IndividualDataIter)
 
 
 def collate_fn(batch):
@@ -45,28 +46,62 @@ def get_noshuffle_datapipe(path, batchsize=1, dataformat="npy"):
     return data
 
 
-def count_samples_in_pipeline(datap, pipeline):
+def get_lister_dp(path, dataformat="npy"):
+    if dataformat == "npy":
+        lister = dp.iter.FileLister(path)
+    elif dataformat == "zarr":
+        lister = dp.iter.IterableWrapper(glob.glob(os.path.join(path, "*.zarr")))
+    else:
+        raise NotImplementedError(f"Data {dataformat} is not implemented")
+    return lister
+
+
+def count_samples_in_datapipe(file, pipeline):
+    if isinstance(file, str):
+        file = dp.iter.IterableWrapper([file])
+
     count = 0
-    for dd in pipeline(datap):
+    for dd in pipeline(file):
         count += 1
     return count
 
 
 def worker_job_datapipe(
-    input_queue,
-    metadata_sm,
+    input_queue, metadata_sm, metadata_type, fields, allocator, done_number, allocations_queue, pipeline
 ):
-    # TODO: can basically follow worker_job_webdataset with appropriate changes
-    pass
+    # basically follows worker_job_webdataset with appropriate changes
+    metadata = np.frombuffer(metadata_sm.buf, dtype=metadata_type)
+    field_names = metadata_type.names
+    # This `with` block ensures that all the pages allocated have been written
+    # onto the file
+    with allocator:
+        while True:
+            todo = input_queue.get()
+            if todo is None:
+                # No more work
+                break
+
+            file, offset = todo
+            actpipe = dp.iter.IterableWrapper([file])
+            done = 0
+            for i, sample in enumerate(pipeline(actpipe)):
+                done += 1
+                dest_ix = offset + i
+                handle_sample(sample, dest_ix, field_names, metadata, allocator, fields)
+
+            with done_number.get_lock():
+                done_number.value += done
+
+    allocations_queue.put(allocator.allocations)
 
 
 class DataPipeWriter(DatasetWriter):
-    def from_datapipe(self, datapipe, pipeline=None):
-        counter = partial(count_samples_in_pipeline, pipeline=pipeline)
-        lengths = thread_map(counter, datapipe)
+    def from_datapipe(self, filelister, pipeline=None):
+        counter = partial(count_samples_in_datapipe, pipeline=pipeline)
+        lengths = list(thread_map(counter, filelister))
         total_len = sum(lengths)
         offsets = np.cumsum([0] + lengths)[:-1]
-        todos = zip(datapipe, offsets)
+        todos = zip(filelister, offsets)
         self._write_common(total_len, todos, worker_job_datapipe, (pipeline,))
 
 
@@ -81,10 +116,16 @@ def convert_climate(args):
 
     writer = DataPipeWriter(args.outfile, {k: NDArrayField(shape=v.shape, dtype=v.dtype) for k, v in data.items()})
 
-    def pipeline(dd):
-        return dd.map(to_tuple)
+    def pipeline(datapipe):
+        if args.dataset == "npy":
+            reader = ERA5Npy
+        elif args.dataset == "zarr":
+            reader = ERA5Zarr
+        return reader(datapipe, variables=["t", "u10", "v10"]).batch(1).collate(collate_fn).map(to_tuple)
 
-    writer.from_datapipe(datap, pipeline=pipeline)
+    filedp = get_lister_dp(args.datapath, dataformat=args.dataset)
+
+    writer.from_datapipe(filedp, pipeline=pipeline)
 
 
 def parse_args():
