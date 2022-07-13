b8c34cd3ccf71cb5575198ae64483c72f80fdde0
rejuvyesh <mail@rejuvyesh.com>
2022-07-13 18:11:43 +0000

    fix for consistency

--- src/compare_climate.py
+++ src/compare_climate.py
@@ -9,15 +9,9 @@ import torch
 import torchdata.datapipes as dp
 from torch.utils.data import DataLoader
 
-from climate.era5_datapipe import (
-    ERA5,
-    NAME_MAP,
-    ERA5Forecast,
-    ERA5Npy,
-    ERA5Zarr,
-    IndividualDataIter,
-    IndividualForecastDataIter,
-)
+from climate.era5_datapipe import (ERA5, NAME_MAP, ERA5Forecast, ERA5Npy,
+                                   ERA5Zarr, IndividualDataIter,
+                                   IndividualForecastDataIter)
 
 # NPY = False
 # NPY_PATH = "/mnt/data/1.40625/_yearly_np"
@@ -87,7 +81,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="FFCV options")
     parser.add_argument("--dataset", type=str, default="npy", help="Dataset to use for benchmarking")
     parser.add_argument("--datapath", type=str, default=None, help="Path to dataset")
-    parser.add_argument("--batchsize", type=int, default=32, help="Batchsize for dataloader")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batchsize for dataloader")
     parser.add_argument("--num_workers", type=int, default=1, help="Number of workers for dataloader")
     parser.add_argument("--use", type=str, default="forecast", help="Use forecast or pretrain")
     args = parser.parse_args()
@@ -97,7 +91,7 @@ def parse_args():
 def benchmark(args):
     print("===== Benchmarking =====")
     print(f"Dataset: {args.dataset}\n \t {args.datapath}")
-    data = get_datapipe(args.datapath, args.batchsize, args.use, args.dataset)
+    data = get_datapipe(args.datapath, args.batch_size, args.use, args.dataset)
     dl = DataLoader(data, batch_size=None, num_workers=args.num_workers)
     time_copy = 0.0
     num_batches = 0
@@ -126,7 +120,7 @@ def benchmark(args):
     print(f"{time_per_batch_without_first:.3f} secs per batch without counting first batch")
     print(f"{time_copy_per_batch:.3f} secs per batch for copying from cpu to gpu")
     mlflow.log_metric(key="num_workers", value=args.num_workers, step=0)
-    mlflow.log_metric(key="batch_size", value=args.batchsize, step=0)
+    mlflow.log_metric(key="batch_size", value=args.batch_size, step=0)
     mlflow.log_metric(key="time_per_batch_without_first", value=time_per_batch_without_first, step=0)
     mlflow.log_metric(key="time_per_batch", value=time_per_batch, step=0)
     mlflow.log_metric(key="time_per_batch", value=time_per_batch, step=0)
